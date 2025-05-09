9d2ee8f4c868af6dd9b328d03727275d09d9b8e6
Michael Ouma <Oumamichael108@gmail.com>
2025-05-09 21:40:43 +0300

    Lab

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-import ipdb
+import pdb  # Built-in debugger
 
 def plus_two(num):
-    num + 2
-    return num
+    return num + 2
+
+def tracing_the_function():
+    pdb.set_trace()  # Debugging breakpoint
+
+    inside_the_function = "We're inside the function"
+    print(inside_the_function)
+
+    print("We're about to stop because of pdb!")
+    pdb.set_trace()  # Second debugging breakpoint
+    
+    this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
+    print(this_variable_hasnt_been_interpreted_yet)
+
+if __name__ == "__main__":
+    tracing_the_function()
